46f614bbb180394eff8d7b04672747e83129ead5
BeedPro <annoor28@hotmail.co.uk>
2023-01-14 14:36:36 +0000

    about prototyped

--- static/js/base.js
+++ static/js/base.js
@@ -6,20 +6,3 @@
 //       x.className = "topnav";
 //     }
 //   }
-
-// Get the navbar element
-let navbar = document.querySelector('.navbar');
-
-// Get the current scroll position
-let prevScrollpos = window.pageYOffset;
-
-// Listen for the scroll event
-window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        navbar.classList.remove("navbar-hidden");
-    } else {
-        navbar.classList.add("navbar-hidden");
-    }
-    prevScrollpos = currentScrollPos;
-}
